29140462364dd22d2d8e967775f14ca4be9e0c87
Humberto845 <humbertoclara696@gmail.com>
2017-04-26 14:48:05 -0500

    4 subida
    Aplicacion 4

--- js/acciones.js
+++ js/acciones.js
@@ -25,5 +25,5 @@ $(document).ready(function (e){
 		
 });//document
 function escribehistoria(accion){
-		$('#eHistoria').append('<li>'+action+'</li>');
+		$('#eHistoria').append('<li>'+accion+'</li>');
 }
